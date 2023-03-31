d8161863c81edbb6bf53ac980d0970981c531da4
Hao Chen <hchen4@lanec1.compbio.cs.cmu.edu>
2023-03-31 15:07:55 -0400

    examples

---
+++ large_scale.py
@@ -0,0 +1,5 @@
+from src import scs
+
+bin_file = 'data/Mouse_brain_Adult_GEM_bin1_sub.tsv'
+image_file = 'data/Mouse_brain_Adult_sub.tif'
+scs.segment_cells(bin_file, image_file, align='rigid', patch_size=1200)
